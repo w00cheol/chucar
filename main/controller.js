7772ff75515acbcb651650a60863f1f16417def8
w00cheol <kw8384@gmail.com>
2022-02-09 22:57:00 +0900

    test

--- main/controller.js
+++ main/controller.js
@@ -302,7 +302,7 @@ exports.logout = (req,res) => {
 
 exports.contractSend = async (req,res) => { //견적서 전송
     try{
-        console.log(req.headers.Authorization);
+        console.log(req);
         console.log('contractsend')
         // console.log(req.headers.Authorization);
         getStatus = await axios.get('http://34.64.207.117:3000/checkToken', {
