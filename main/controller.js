f8705b606aee430710fafb159be62fb32d060c07
w00cheol <kw8384@gmail.com>
2022-02-19 03:23:44 +0900

    test

--- main/controller.js
+++ main/controller.js
@@ -29,8 +29,9 @@ exports.show = (req, res) =>{
 }
 exports.isDealer = (req, res) => { // 딜러인지 알려주는 함수 딜러 1, 고객 0
     console.log('is Dealer');
+    var date = new Date();
     findId = req.params.usr_id;
-    con.query(`SELECT pro_id from promst where pro_id = '${findId}' limit 1`, (error, rows) => {
+    con.query(`SELECT pro_id from promst where pro_id = '${findId}' and pro_end >= ${date.getTime()/1000} limit 1`, (error, rows) => {
         if(error) return res.status(404).json({err: 'Undefined error!'});
         if(!!rows[0]) res.json(1);
         else res.json(0);
