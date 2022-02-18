7726cac88dbbad6e603bdf2566d764861ffd930c
w00cheol <kw8384@gmail.com>
2022-02-19 04:40:07 +0900

    test

--- main/controller.js
+++ main/controller.js
@@ -32,7 +32,7 @@ exports.isDealer = (req, res) => { // 딜러인지 알려주는 함수 딜러 1,
     var date = new Date();
     findId = req.params.usr_id;
     console.log(findId);
-    if(findId==undefined) res.json(undefined);
+    if(findId=='undefined') res.json(undefined);
     con.query(`SELECT pro_id from promst where pro_id = '${findId}' and pro_end >= ${date.getTime()/1000} limit 1`, (error, rows) => {
         if(error) return res.status(404).json({err: 'Undefined error!'});
         if(rows[0]) res.json(1);
