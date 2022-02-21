2cabf3457bde58aca6d4ac8b25bf6ee808d8e979
w00cheol <kw8384@gmail.com>
2022-02-22 00:37:29 +0900

    test

--- main/controller.js
+++ main/controller.js
@@ -529,7 +529,7 @@ exports.getMerchantUid = async (req, res) => {
     const {code, customer_uid} = req.body;
     console.log(code);
     console.log(customer_uid);
-    if(!customer_uid) return res.json(customer_uid);
+    if(customer_uid=='undefined') return res.json(customer_uid);
     else {
         con.query(`select CONCAT('${code}','${customer_uid}',
                    GET_ODNO('${code}','${customer_uid}')) uid from dual`, (error, rows, fields) => {
