615e179909fc221f3ea8dd361765996af54caf25
w00cheol <kw8384@gmail.com>
2022-02-24 04:50:53 +0900

    test

--- main/controller.js
+++ main/controller.js
@@ -64,8 +64,7 @@ exports.isSubscribe = (req, res) => { // 구독중인지 알려주는 함수 딜
     var date = new Date();
     findId = req.params.usr_id;
     console.log(findId);
-    if(findId=='undefined') res.json(findId);
-    else con.query(`SELECT pro_id from promst where pro_id = '${findId}' and pro_end >= ${date.getTime()/1000} limit 1`, (error, rows) => {
+    con.query(`SELECT pro_id from promst where pro_id = '${findId}' and pro_end >= ${date.getTime()/1000} limit 1`, (error, rows) => {
         if(error) return res.status(404).json({err: 'Undefined error!'});
         if(rows[0]) res.json(1);
         else res.json(0);
@@ -75,7 +74,8 @@ exports.isDealer = (req, res) => { // 딜러인지 알려주는 함수 딜러 1,
     console.log('is Dealer');
     findId = req.params.usr_id;
     console.log(findId);
-    con.query(`SELECT pro_id from promst where pro_id = '${findId}'`, (error, rows) => {
+    if(findId=='undefined') res.json(findId);
+    else con.query(`SELECT pro_id from promst where pro_id = '${findId}'`, (error, rows) => {
         if(error) return res.status(404).json({err: 'Undefined error!'});
         if(rows[0]) res.json(1);
         else res.json(0);
